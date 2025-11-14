cac7edb79e6c74a0c00707f76c1bd435323aaf2e
Solal Pirelli <solal.pirelli@gmail.com>
2025-11-14 11:07:57 +0100

    Commit initial

--- app.py
+++
@@ -1,8 +0,0 @@
-from model import LocalDatabaseModel
-from view import ConsoleView
-from presenter import Presenter
-
-Presenter(LocalDatabaseModel(), ConsoleView()).run()
-
-# TODO
-# teach skimming -- make it clear this PR is from a colleague, don't expect them to read the JSON
--- view.py
+++
@@ -1,9 +0,0 @@
-class ConsoleView:
-    def show(self, text):
-        # Affiche `text` sur la console
-        print(text)
-
-    def run(self, presenter, prompt):
-        while True:
-            command = input(prompt)
-            presenter.onInput(command)
